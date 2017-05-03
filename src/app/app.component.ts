334d4478bc1e196d4555ba7a6e805ec69980ec6f
Pallavi Bharwani <p.bharwani@CIMSOLUTIONS.local>
2017-05-03 14:45:56 +0200

    created develop branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
+    <h2>My Hero</h2>
       <ul class="heroes">
         <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
